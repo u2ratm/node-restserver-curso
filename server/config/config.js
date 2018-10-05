2b5c7c19c0c550b52e3cff60c41832b83bb13507
Omar <omar.vazquez@towasoftware.com>
2018-10-05 16:29:38 -0500

    Login Normal

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,18 @@ process.env.PORT = process.env.PORT || 3000
 //===============
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+    // ==============
+    // Vencimiento del token
+    //===============
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30
+
+// ==============
+// Seed de autenticacion
+//===============
+
+process.env.SEED = process.env.SEED || 'este-es-el-seed-de-desarrollo'
+
 
 // ==============
 // Base de datos
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,10 +3,11 @@ const bcrypt = require('bcrypt')
 const _ = require('underscore')
 
 const Usuario = require('../models/usuario')
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion')
 
 const app = express();
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
 
     let desde = req.query.desde || 0
     desde = Number(desde)
@@ -35,7 +36,7 @@ app.get('/usuario', function(req, res) {
         })
 })
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let body = req.body
 
@@ -62,7 +63,7 @@ app.post('/usuario', function(req, res) {
     })
 })
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado'])
@@ -85,7 +86,7 @@ app.put('/usuario/:id', function(req, res) {
 
 })
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id
         //Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
